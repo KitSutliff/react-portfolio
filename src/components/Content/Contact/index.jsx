8b59946274f442c0198a0b84a82cb189a65cb3fe
KitSutliff <38538497+KitSutliff@users.noreply.github.com>
2022-10-26 15:52:09 -0700

    enables snap-to-scroll on js components

---
+++ src/components/Content/Contact/index.jsx
@@ -0,0 +1,16 @@
+
+const Contact = () => {
+    return (
+         
+        <section>
+        <div>
+        <div class="section-heading">Contact</div>
+        <div class="heading-underline"></div>
+        </div>
+        </section>
+
+       
+    )
+}
+
+export default Contact
--- src/components/Content/index.js
+++ src/components/Content/index.js
@@ -3,16 +3,20 @@ import Welcome from './Welcome'
 import Professional from './Professional'
 import Personal from './Personal'
 import BonaFides from './BonaFides'
+import Contact from './Contact'
+
 
 
 const Content = () => {
     return (
 
+        
         <div className="container">
             <Welcome />
             <Professional />
             <Personal />
             <BonaFides />
+            <Contact />
         </div>
        
     )
