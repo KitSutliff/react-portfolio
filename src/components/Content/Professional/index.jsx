40671cfa3c68eb18317a64b6ba57e9866a71f06b
KitSutliff <38538497+KitSutliff@users.noreply.github.com>
2022-11-03 16:13:48 -0700

    adds containers for section content

--- src/components/Content/Professional/index.jsx
+++ src/components/Content/Professional/index.jsx
@@ -7,10 +7,23 @@ const Professional = () => {
                     <div className="heading-underline"></div>
                     <div className="section-subheading">Qumulo</div>
                 </div>
-                {/* <div className="container-section-content">
-                    <div className="left-to-bottom"></div>
-                    <div className="left-to-bottom"></div>
-                </div> */}
+                <div className="container-section-content">
+                    <div className="left-to-bottom">
+                        <div>
+                            <h3>Azure Biller</h3>
+                            <p>Azure BillerRebuilt point-of-sale for all 
+    Qumulo cloud services offered through 
+    Microsoft Azure Marketplace. 
+                            </p>
+                        </div>
+                    </div>
+                    <div className="right-to-top">
+                        <div>
+                            <img src="http://localhost:3000/QumuloBiller.png" alt="billing page image">
+                            </img>
+                        </div>
+                    </div>
+                </div>
             </div>
         </section>
 
