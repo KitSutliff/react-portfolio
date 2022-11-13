41334cbaa7661aba4dd1393d87169eef048f6698
KitSutliff <38538497+KitSutliff@users.noreply.github.com>
2022-11-13 12:27:05 -0800

    adds contact form

--- src/components/Content/Contact/index.jsx
+++ src/components/Content/Contact/index.jsx
@@ -1,13 +1,52 @@
 
+import React from 'react'
 const Contact = () => {
+    const [formStatus, setFormStatus] = React.useState('Send')
+    // SOLUTION 2 COMPONENT
+    const onSubmit = (e) => {
+        e.preventDefault()
+        setFormStatus('Submitting...')
+        const { name, email, message } = e.target.elements
+        let conFom = {
+            name: name.value,
+            email: email.value,
+            message: message.value,
+        }
+        console.log(conFom)
+
+        // ADD EMAIL SUBMISSION COMPONENT HERE
+
+    }
     return (
         <section>
-            <div>
+            <div className="container-contact-section" >
                 <div class="container-contact-heading" id="contact">
                     Contact
                 </div>
-                <div class="heading-underline">
-
+                <div >
+                    <form onSubmit={onSubmit}>
+                        <div>
+                            <label className="form-label" htmlFor="name">
+                                Name
+                            </label>
+                            <input className="form-control" type="text" id="name" required />
+                        </div>
+                        <div>
+                            <label className="form-label" htmlFor="email">
+                                Email
+                            </label>
+                            <input className="form-control" type="email" id="email" required />
+                        </div>
+                        <div>
+                            <label className="form-label" htmlFor="message">
+                                Message
+                            </label>
+                            <textarea className="form-control" id="message" required />
+                        </div>
+                        <button className="btn btn-danger" type="submit">
+                            {formStatus}
+                        </button>
+                    </form>
                 </div>
             </div>
         </section>
